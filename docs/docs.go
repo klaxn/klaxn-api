00779b4bd46008c41abb73b72f03c0944d457d6f
petetanton <petetanton@users.noreply.github.com>
2023-05-04 11:48:28 +0000

    Update swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -1213,8 +1213,6 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
-	LeftDelim:        "{{",
-	RightDelim:       "}}",
 }
 
 func init() {
